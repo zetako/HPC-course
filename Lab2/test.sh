b058a90ce7ef39fa5ad2e0515c85d56ecaccd6c9
zetako <zetako@outlook.com>
2020-10-09 21:45:44 +0800

    202010092144 NUC | Fix makefile

--- Lab2/test.sh
+++ Lab2/test.sh
@@ -2,17 +2,17 @@
 
 make point2point
 echo '512 512 512' > arg.txt
-mpirun -n 1 ./point2point < arg.txt
-mpirun -n 2 ./point2point < arg.txt
-mpirun -n 4 ./point2point < arg.txt
-mpirun -n 8 ./point2point < arg.txt
+mpirun --oversubscribe -n 1 ./point2point < arg.txt
+mpirun --oversubscribe -n 2 ./point2point < arg.txt
+mpirun --oversubscribe -n 4 ./point2point < arg.txt
+mpirun --oversubscribe -n 8 ./point2point < arg.txt
 echo '1024 1024 1024' > arg.txt
-mpirun -n 1 ./point2point < arg.txt
-mpirun -n 2 ./point2point < arg.txt
-mpirun -n 4 ./point2point < arg.txt
-mpirun -n 8 ./point2point < arg.txt
+mpirun --oversubscribe -n 1 ./point2point < arg.txt
+mpirun --oversubscribe -n 2 ./point2point < arg.txt
+mpirun --oversubscribe -n 4 ./point2point < arg.txt
+mpirun --oversubscribe -n 8 ./point2point < arg.txt
 echo '2048 2048 2048' > arg.txt
-mpirun -n 1 ./point2point < arg.txt
-mpirun -n 2 ./point2point < arg.txt
-mpirun -n 4 ./point2point < arg.txt
-mpirun -n 8 ./point2point < arg.txt
+mpirun --oversubscribe -n 1 ./point2point < arg.txt
+mpirun --oversubscribe -n 2 ./point2point < arg.txt
+mpirun --oversubscribe -n 4 ./point2point < arg.txt
+mpirun --oversubscribe -n 8 ./point2point < arg.txt
